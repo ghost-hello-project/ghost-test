1a83f25465f6459b75359004100c4e5a167d701b
laolang2016 <xiaodaima20162163.com>
2024-09-30 06:14:52 +0800

    rest-template-study

--- rest-template-study/src/test/java/com/laolang/jx/RestTemplateSimpleTest.java
+++ rest-template-study/src/test/java/com/laolang/jx/RestTemplateSimpleTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
